0441cbeb6f827fe332eed1245e5e22b1841ffde0
Marek <repinski@hotmail.com>
2020-09-16 10:48:44 +0200

    Added button

--- app/src/main/java/se/ctescape/localization/MainActivity.kt
+++ app/src/main/java/se/ctescape/localization/MainActivity.kt
@@ -2,10 +2,22 @@ package se.ctescape.localization
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var tv : TextView
+    val name = "David"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tv = findViewById<TextView>(R.id.tvTitle)
+    }
+
+    fun onClickButton(v : View){
+        Log.d("AIK","Button pressed")
+        tv.text = getString(R.string.great, name)
     }
 }
